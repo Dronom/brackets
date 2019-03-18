fdb181a40f1058b8f5515fdd7199cf7ad6b12dfd
Roman <dronymys@mail.ru>
2019-03-18 16:25:15 +0300

    feat: make brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++){
+      if (str[0] == bracketsConfig[i][1] && bracketsConfig[i][0] != bracketsConfig[i][1] ) {
+        return false;}
+    }
+
+  for (let i = 0; i < str.length; i++) { 
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) {
+        if (str[i] != bracketsConfig[j][1]) {
+          stack.push(str[i]);
+          break;
+        }
+        else if(stack[stack.length - 1] == str[i]) {
+          stack.pop();
+          break;
+        }
+        else{
+          stack.push(str[i]);
+          break;
+        }
+      }
+      else if(str[i] == bracketsConfig[j][1]) {
+        if (stack[stack.length - 1] == bracketsConfig[j][0]) {
+          stack.pop();
+          break;
+        }
+      else return false;
+      } 
+    }
+  }
+  if (stack.length == 0) {
+  return true;
+  }
+  else return false;
 }
